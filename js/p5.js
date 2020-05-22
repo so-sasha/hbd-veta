1c4fe3b9efa80b201b75b49d1d831d2b013fd2d9
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-05-22 20:29:16 +0300

    more doughnuts

--- js/p5.js
+++ js/p5.js
@@ -2,79 +2,69 @@ let ele;
 
 let button;
 
-let myFont;
-
 function setup() {
 
   createCanvas(1440, 900, WEBGL);
 
   ele = createAudio('music/hbd.mp3');
-  // here we set the element to autoplay
-  // The element will play as soon
-  // as it is able to do so.
   ele.autoplay(true);
 
-  button = createButton('next');
+  button = createButton('happy birthday!');
   button.position(19, 19);
   button.mousePressed();
 
   frameRate(10);
 
-  loadFont('../fonts/homemadeapple.ttf', drawText);
-}
-
-function drawText(font) {
-  fill('#ED225D');
-  textFont(font, 36);
-  text('happy birthday', 10, 50);
 }
 
 let value = '#F09696';
-
-let words = ['hey', 'you', 'have', 'a', 'nice', 'day'];
-let index = 0;
 
 function draw() {
   background(value);
 
   noStroke();
+  translate(mouseX - width/2, mouseY - height/2)
+  let locX = mouseX - width / 2;
+  let locY = mouseY - height / 2;
+  ambientLight(300, 180, 300);
+  pointLight(0, 0, 255, -200, 0, 200);
+  pointLight(300, 100, 255, 0, 200, 0)
+  specularMaterial(100);
+  translate(50, 0, 0);
+  shininess(20);
   fill(240, 150, 150);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(30, 15);
-
-  // noStroke();
-  // fill(240, 150, 150);
-  // rotateX(frameCount * 0.01);
-  // rotateY(frameCount * 0.01);
-  // torus(30, 15);
+  rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.05);
+  torus(20, 10);
 
   noStroke();
+  translate(mouseX - width/2.5, mouseY - height/2.5)
   fill(240, 150, 150);
-  rotateX(frameCount * 0.0001);
-  rotateY(frameCount * 0.0001);
-  torus(mouseX, 30, mouseY, 10);
+  rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.05);
+  torus(20, 10);
 
-  textFont('Georgia');
-  textSize(100);
-  text(words[index], 10, 150);
+  noStroke();
+  translate(mouseX - width/1.8, mouseY - height/1.8)
+  fill(240, 150, 150);
+  rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.05);
+  torus(20, 10);
+
+  noStroke();
+  translate(mouseX - width/1.5, mouseY - height/1.5)
+  fill(240, 150, 150);
+  rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.05);
+  torus(20, 10);
+
+
 }
-
-// function mousePressed() {
-//   clear();
-// }
 
 function mouseReleased() {
   if (value === '#F09696') {
     value = '#8b7ca9';
   } else {
     value = '#F09696';
-  }
-}
-
-function mouseMoved() {
-  index += 1;
-  if (index === words.length) {
-    index = 0;
   }
 }
