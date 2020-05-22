99c9a29014948499c93d53581f1ff5e0f6d1d421
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-05-22 21:06:20 +0300

    update

--- js/p5.js
+++ js/p5.js
@@ -26,80 +26,114 @@ function draw() {
   translate(mouseX - width/2, mouseY - height/2)
   let locX = mouseX - width / 2;
   let locY = mouseY - height / 2;
-  ambientLight(300, 180, 300);
-  pointLight(0, 0, 255, -200, 0, 200);
-  pointLight(300, 100, 255, 0, 200, 0)
-  specularMaterial(100);
+  ambientLight(500, 1000, 300);
+  pointLight(100, 50, 255, -100, 0, 100);
+  pointLight(350, 50, 100, 0, 200, 0)
+  specularMaterial(300);
   translate(50, 0, 0);
   shininess(20);
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.03);
   rotateZ(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/2.5, mouseY - height/2.5)
-  fill(240, 150, 150);
-  rotateX(frameCount * 0.03);
+  fill(255,147,147);
+  rotateY(frameCount * 0.03);
   rotateZ(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/1.8, mouseY - height/1.8)
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.03);
-  rotateZ(frameCount * 0.05);
+  rotateY(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/1.5, mouseY - height/1.5)
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.03);
   rotateZ(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
-  translate(mouseX - width/3, mouseY - height/3)
-  fill(240, 150, 150);
+  translate(mouseX - width/2, mouseY - height/2)
+  fill(255,147,147);
   rotateX(frameCount * 0.05);
-  rotateZ(frameCount * 0.05);
+  rotateY(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/3, mouseY - height/3)
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.01);
   rotateZ(frameCount * 0.01);
   torus(20, 10);
 
   noStroke();
-  translate(mouseX - width/2.8, mouseY - height/2.8)
-  fill(240, 150, 150);
+  translate(mouseX - width/2.4, mouseY - height/2.4)
+  fill(255,147,147);
   rotateX(frameCount * 0.03);
   rotateZ(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/1.5, mouseY - height/1.5)
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.02);
   rotateZ(frameCount * 0.02);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/5, mouseY - height/5)
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.03);
-  rotateZ(frameCount * 0.05);
+  rotateY(frameCount * 0.05);
   torus(20, 10);
 
   noStroke();
   translate(mouseX - width/2.5, mouseY - height/2.5)
-  fill(240, 150, 150);
+  fill(255,147,147);
   rotateX(frameCount * 0.04);
   rotateZ(frameCount * 0.04);
   torus(20, 10);
 
+  noStroke();
+  translate(mouseX - width/2.2, mouseY - height/2.2)
+  fill(255,147,147);
+  rotateX(frameCount * 0.04);
+  rotateZ(frameCount * 0.04);
+  torus(20, 10);
+
+  noStroke();
+  translate(mouseX - width/2.2, mouseY - height/2.2)
+  fill(255,147,147);
+  rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.01);
+  torus(20, 10);
+
+  noStroke();
+  translate(mouseX - width/1.3, mouseY - height/1.3)
+  fill(255,147,147);
+  rotateX(frameCount * 0.04);
+  rotateY(frameCount * 0.04);
+  torus(20, 10);
+
+  noStroke();
+  translate(mouseX - width/1.9, mouseY - height/2)
+  fill(255,147,147);
+  rotateX(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  torus(20, 10);
+
+  noStroke();
+  translate(mouseX - width/1.8, mouseY - height/1.8)
+  fill(255,147,147);
+  rotateX(frameCount * 0.03);
+  rotateY(frameCount * 0.05);
+  torus(20, 10);
 
 }
 
